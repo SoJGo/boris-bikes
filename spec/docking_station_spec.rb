b6e22140e81a56a8cee28aada7992c66205234d6
SophieGowen <sophie.j.gowen@gmail.com>
2021-12-11 10:04:46 +0000

    Refactor docking_station_spec

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,30 +2,22 @@ require 'docking_station'
 
 describe DockingStation do
     it { is_expected.to respond_to(:release_bike) }
-    
-    # it 'releases working bike' do
-    #   bike = subject.release_bike
-    #   expect(bike).to be_working
-    # end
-
-    it 'releases working bike if a bike has been docked' do
-      bike = Bike.new
-      subject.dock(bike)
-      released_bike = subject.release_bike
-      expect(released_bike).to eq bike
-    end
-
-  describe "#dock" do
     it { is_expected.to respond_to(:dock) }
     
-    it "adds a bike to docking_station" do
-      expect(subject.dock(Bike)).to eq Bike
+  describe '#dock' do
+    it 'adds a bike to docking_station' do
+      expect(subject.dock(bike)).to eq bike
     end
   end
 
-  describe "#release_bike" do
-  it "should get an error if there is not any bike in the station" do
-  expect {subject.release_bike}.to raise_error
-  end
+  describe '#release_bike' do
+    it 'should get an error if there is not a bike in the dock' do
+      expect { subject.release_bike }.to raise_error "Sorry, No Bikes Available"
+    end
+
+    it 'releases working bike if a bike has been docked' do
+      subject.dock(Bike.new)
+      expect(subject.release_bike.working?).to eq true
+    end
   end
 end
